9449b3ef50981801badec050c722b26d45fffa28
Greg Pearce <gregrpearce@gmail.com>
2017-03-26 22:12:41 +0100

    add firebase + add basic notification service

---
+++ app/src/main/java/com/grepx/tflnotify/MyFirebaseInstanceIDService.java
@@ -0,0 +1,6 @@
+package com.grepx.tflnotify;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/grepx/tflnotify/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.grepx.tflnotify;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}

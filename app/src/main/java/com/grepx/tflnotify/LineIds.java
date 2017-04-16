41f40c74689c45049723bec48bbb5059fe0560ef
Greg Pearce <gregrpearce@gmail.com>
2017-04-16 22:25:18 +0100

    refactor line statuses and add proper line names

---
+++ app/src/main/java/com/grepx/tflnotify/LineIds.java
@@ -0,0 +1,17 @@
+package com.grepx.tflnotify;
+
+public class LineIds {
+  public static final String Bakerloo = "bakerloo";
+  public static final String Central = "central";
+  public static final String Circle = "circle";
+  public static final String District = "district";
+  public static final String Dlr = "dlr";
+  public static final String HammersmithCity = "hammersmith-city";
+  public static final String Jubilee = "jubilee";
+  public static final String LondonOverground = "london-overground";
+  public static final String Metropolitan = "metropolitan";
+  public static final String Northern = "northern";
+  public static final String Piccadilly = "piccadilly";
+  public static final String Victoria = "victoria";
+  public static final String WaterlooCity = "waterloo-city";
+}
--- app/src/main/java/com/grepx/tflnotify/MainApplication.java
+++ app/src/main/java/com/grepx/tflnotify/MainApplication.java
@@ -6,18 +6,18 @@ import com.google.firebase.messaging.FirebaseMessaging;
 public class MainApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
-    FirebaseMessaging.getInstance().subscribeToTopic("bakerloo");
-    FirebaseMessaging.getInstance().subscribeToTopic("central");
-    FirebaseMessaging.getInstance().subscribeToTopic("circle");
-    FirebaseMessaging.getInstance().subscribeToTopic("district");
-    FirebaseMessaging.getInstance().subscribeToTopic("dlr");
-    FirebaseMessaging.getInstance().subscribeToTopic("hammersmith-city");
-    FirebaseMessaging.getInstance().subscribeToTopic("jubilee");
-    FirebaseMessaging.getInstance().subscribeToTopic("london-overground");
-    FirebaseMessaging.getInstance().subscribeToTopic("metropolitan");
-    FirebaseMessaging.getInstance().subscribeToTopic("northern");
-    FirebaseMessaging.getInstance().subscribeToTopic("piccadilly");
-    FirebaseMessaging.getInstance().subscribeToTopic("victoria");
-    FirebaseMessaging.getInstance().subscribeToTopic("waterloo-city");
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Bakerloo);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Central);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Circle);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.District);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Dlr);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.HammersmithCity);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Jubilee);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.LondonOverground);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Metropolitan);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Northern);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Piccadilly);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.Victoria);
+    FirebaseMessaging.getInstance().subscribeToTopic(LineIds.WaterlooCity);
   }
 }
